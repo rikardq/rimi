5e210a9e1832b673b541d46d7cc9657a509d2dee
mceder <mcederberg@gmail.com>
2013-10-06 11:52:59 +0200

    Adding more test data for all tables

--- insert_test_data.py
+++ insert_test_data.py
@@ -25,4 +25,30 @@ db.owner_of_leason.insert(leason_id=2,instructor_id=2)
 db.owner_of_leason.insert(leason_id=3,instructor_id=1)
 db.owner_of_leason.insert(leason_id=4,instructor_id=2)
 db.owner_of_leason.insert(leason_id=4,instructor_id=1)
+
+db.rebooking.insert(id_leason=3,id_customer=1,leason_date="2013-10-30",approval="notyet")
+db.rebooking.insert(id_leason=3,id_customer=1,leason_date="2013-9-4",approval="notyet")
+
+db.cancelled_leasons.insert(id_customer=1,id_leason=6,cancelled_date="2013-09-14",when_cancelled="2013-10-06 11:33:57")
+db.cancelled_leasons.insert(id_customer=1,id_leason=6,cancelled_date="2013-11-02",when_cancelled="2013-10-06 11:33:57")
+
+db.leasons_history.insert(id_customer=1,id_leason=6,id_semester=1,leason_length=60,leason_date="2013-08-17")
+db.leasons_history.insert(id_customer=1,id_leason=6,id_semester=1,leason_length=60,leason_date="2013-08-24")
+db.leasons_history.insert(id_customer=1,id_leason=6,id_semester=1,leason_length=60,leason_date="2013-08-31")
+db.leasons_history.insert(id_customer=1,id_leason=6,id_semester=1,leason_length=60,leason_date="2013-09-07")
+db.leasons_history.insert(id_customer=1,id_leason=6,id_semester=1,leason_length=60,leason_date="2013-09-21")
+db.leasons_history.insert(id_customer=1,id_leason=6,id_semester=1,leason_length=60,leason_date="2013-09-28")
+
+db.black_dates.insert(id_semester=1,black_date="2013-08-20")
+db.black_dates.insert(id_semester=1,black_date="2013-09-16")
+db.black_dates.insert(id_semester=1,black_date="2013-11-28")
+
+db.horse.insert(name="Balder",status="Aktiv",horse_type="Ponny")
+db.horse.insert(name="Fanta",status="Aktiv",horse_type="Ponny")
+db.horse.insert(name="Sandra",status="Aktiv",horse_type="Ponny")
+
+db.reserved_horses.insert(id_customer=1,id_horse=2,id_leason=4,reserved_date="2013-10-31")
+db.reserved_horses.insert(id_customer=1,id_horse=1,id_leason=4,reserved_date="2013-11-14")
+
+
 db.commit()
