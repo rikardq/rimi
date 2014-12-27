b94ed87a19033090deed384222fbcbca54bc9b27
Mikael Cederberg <mcederberg@gmail.com>
2014-12-27 19:45:08 +0100

    moving display out of controller, moving data into dict. However, all the data is now unsorted and displays seemingly random dates..?

--- controllers/instructor.py
+++ controllers/instructor.py
@@ -15,56 +15,69 @@ if session.num_of_date_rows is None:
 else:
     num_of_date_rows = int(session.num_of_date_rows)
 
-
-"""
-What should be here?
-What can an instructor do?
-
-- Can make announcements to its groups via Infoboard system
-- Can cancel individual dates on one of their leasons
-- See the leasons that they are responsible for, default today
-- Administrate horse-selections to its leasons/riders
-"""
-
 def index():
     week_display()
     ### The variable startday determines which day to base the view dates from. Default is today
     if session.startday is None:
         session.startday = today
-    week_display()
-
 
     # Set variable from session
     startday = session.startday 
     master = {}
+    # column division for bootstrap. 
+    col_division = 12/num_of_date_rows
+
 
     for day in dayz:
         leasons_for_this_day = []
         for leason in leasons:
             if len(db((db.leason.id==leason.leason_id)&(db.leason.week_day==day)).select()) > 0:
                 leasons_for_this_day.append(leason.leason_id)
+                # Add weekday to master dict since we have a leason for it ,unless its in there already
+                if day not in master:
+                    master[day] = {}
 
-        if len(leasons_for_this_day) > 0:
-            master[day] = [] 
+        for leason in leasons_for_this_day: 
+            master[day][leason] = {}
+            # Find first available date for this weekday
+            startdate = get_firstdate_weekday(reverse_translate_weekday(db.leason[leason].week_day), startday)
 
-            for leason in leasons_for_this_day: 
-                # Find first available date for this weekday
-                startdate = get_firstdate_weekday(reverse_translate_weekday(db.leason[leason].week_day), startday)
+            startpoint = 0 
 
-                a = Viewleason(leason)
-                startpoint = 0 
+            while num_of_date_rows > startpoint: 
+                master[day][leason][startdate] = {}
+                riders, available_slots = prepped_leason_info(leason, startdate)
+                master[day][leason][startdate]["available_slots"] = available_slots
+                master[day][leason][startdate]["riders"] = riders 
 
-                while num_of_date_rows > startpoint: 
-                    # Build a column for this particular weekday, leason and finally actual date
-                    a.build_divs(startdate)
-                    startpoint = startpoint + 1
-                    startdate = startdate + timedelta(+7)
+                startpoint = startpoint + 1
+                startdate = startdate + timedelta(+7)
 
-                a.end_div()
-                # add to master dict for this day 
-                master[day].append(a.divz)
 
-    return dict(startday=startday,master=master) 
+    return dict(col_division=col_division, startday=startday,master=master) 
+
+
+def prepped_leason_info(leason_id, leason_date):
+    # returns a dict with available slots and 
+    # returns a dict of customers with their status
+    # 
+    data = {}
+    num_riders,num_rebooks,num_canx,num_total,reg_riders,canx_riders,rebook_riders,leason_time = leason_info(leason_date,leason_id)
+    max_customers = int(db(db.leason.id==leason_id).select()[0]['max_customers'])
+    available_slots = max_customers - num_riders - num_rebooks + num_canx 
+
+    for reg_rider in reg_riders:
+        if reg_rider in canx_riders:
+            divclass = "danger"
+        else:
+            divclass = "success"
+        data[reg_rider["first_name"] + " " + reg_rider["last_name"]] = divclass
+
+    for rebook_rider in rebook_riders:
+        divclass="info"
+        data[rebook_rider["first_name"] + " " + rebook_rider["last_name"]] = divclass
+
+    return data, available_slots
 
 def change_num_of_date_rows(): 
     # Function to change how many columns or date rows should be displayed. 
@@ -107,58 +120,3 @@ def week_display():
     session.forward = forward
     session.week = week
     session.year = year 
-
-
-class Viewleason:
-    'Common class to prepare the view for a leason'
-    def __init__(self, leason_id):
-        self.leason_id = leason_id
-        self.time = str(db(db.leason.id == leason_id).select(db.leason.leason_time)[0]['leason_time'])[:5]
-        # start to construct the div, first heading is the time
-        # 
-        self.divz = """
-        <div class="dg" data-toggle="collapse" data-target="#%s" aria-expanded="true" aria-controls="%s">
-            <h4>%s</h4>
-        </div>
-
-        <div id="%s" class="collapse">
-            <div class="row">\n""" % (self.leason_id, self.leason_id, self.time, self.leason_id)
-
-    def end_div(self):
-        self.divz += """
-            </div>
-        </div>"""
-
-    def build_divs(self, leason_date):
-        self.leason_date = leason_date
-        num_riders,num_rebooks,num_canx,num_total,reg_riders,canx_riders,rebook_riders,leason_time = leason_info(self.leason_date,self.leason_id)
-        max_customers = int(db(db.leason.id==self.leason_id).select()[0]['max_customers'])
-        available_slots = max_customers - num_riders - num_rebooks + num_canx 
-        col_division = 12/num_of_date_rows 
-
-        self.divz += """
-                <div class="col-md-%s">
-                    <div class="list-group">
-                        <li class="list-group-item list-group-item-warning">%s</li>\n""" % (col_division, self.leason_date)
-        for reg_rider in reg_riders:
-            if reg_rider in canx_riders:
-                divclass = "danger"
-            else:
-                divclass = "success"
-
-            self.divz += " <a href='#' class='list-group-item list-group-item-%s'>%s %s</a>\n" % (divclass, reg_rider['first_name'], reg_rider['last_name'])
-
-        for rebook_rider in rebook_riders:
-            divclass="info"
-            self.divz += " <a href='#' class='list-group-item list-group-item-%s'>%s %s</a>\n" % (divclass, rebook_rider['first_name'], rebook_rider['last_name'])
-
-        # Finish the row with empty slots
-        if available_slots > 0:
-            av_div = "          <a href='#' class='list-group-item list-group-item-active'>Ledig plats</a>\n"
-            self.divz +=  av_div * available_slots
-
-        # finish this column
-        self.divz += """
-                        </li>
-                    </div>
-                </div>"""
